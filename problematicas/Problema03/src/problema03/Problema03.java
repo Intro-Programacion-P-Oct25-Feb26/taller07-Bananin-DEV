47283a1c1fe4b16eaf1eeb14c1567e7c7eaa51d3
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-11-20 12:17:14 -0500

    Problema 03

--- problematicas/Problema03/src/problema03/Problema03.java
+++ problematicas/Problema03/src/problema03/Problema03.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package problema02;
+package problema03;
 
 import java.util.Scanner;
 import java.util.Locale;
@@ -11,7 +11,7 @@ import java.util.Locale;
  *
  * @author UTPL
  */
-public class Problema02 {
+public class Problema03 {
 
     /**
      * @param args the command line arguments
@@ -20,14 +20,13 @@ public class Problema02 {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        int numero = 2;
-        int valor_suma = 4;
+        int num = 2;
+        int valor_suma = 3;
         String mensaje = "";
+        while (num <= 37) {
+            mensaje = String.format("%s\n%S", mensaje, num);
 
-        while (numero <= 110) {
-            mensaje = String.format("%s\n%S", mensaje, numero);
-
-            numero = numero + valor_suma;
+            num = num + valor_suma;
             valor_suma = valor_suma + 2;
 
         }
